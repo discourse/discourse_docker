570c677faa3f6db95893e82337697e5c42db09cf
Sam Saffron <sam.saffron@gmail.com>
2015-11-18 19:23:23 -0500

    bump version

--- image/build.rb
+++ image/build.rb
@@ -2,13 +2,14 @@
 #
 require 'pty'
 
-$version = "1.0.14"
+$version = "1.0.15"
 
 $docker_squash = "https://github.com/jwilder/docker-squash/releases/download/v0.2.0/docker-squash-linux-amd64-v0.2.0.tar.gz"
 
 $base_image = "discourse/base:#{$version}"
 $image = "discourse/discourse:#{$version}"
 $fast_switch= "discourse/discourse_fast_switch:#{$version}"
+$test = "discourse/discourse_test:#{$version}"
 
 if ENV["USER"] != "root"
   STDERR.puts "Build script must be ran as root due to docker-squash"
@@ -50,3 +51,4 @@ end
 build("base",$base_image,true)
 build("discourse",$image,false)
 build("discourse_fast_switch",$fast_switch,false)
+build("discourse_test",$test,false)
