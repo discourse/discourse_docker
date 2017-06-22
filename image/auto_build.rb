38cff919080ab2a8e0a535cb587de6167cda7f6d
Rafael dos Santos Silva <xfalcox@gmail.com>
2017-06-21 23:45:19 -0300

    FEATURE: Build a public test image every week

--- image/auto_build.rb
+++ image/auto_build.rb
@@ -5,8 +5,9 @@ require 'optparse'
 
 images = {
   base: { name: 'base', tag: "discourse/base:build", squash: true },
-  discourse_test: { name: 'discourse_test', tag: "discourse/discourse_test:build", squash: false},
-  discourse_dev: { name: 'discourse_dev', tag: "discourse/discourse_dev:build", squash: false }
+  discourse_test_build: { name: 'discourse_test', tag: "discourse/discourse_test:build", squash: false},
+  discourse_dev: { name: 'discourse_dev', tag: "discourse/discourse_dev:build", squash: false },
+  discourse_test_public: { name: 'discourse_test', tag: "discourse/discourse_test:release", squash: true, extra_args: ' --build-arg tag=release '}
 }
 
 def run(command)
@@ -26,7 +27,7 @@ def run(command)
 end
 
 def build(image)
-  lines = run("cd #{image[:name]} && docker build . --no-cache --tag #{image[:tag]} #{image[:squash] ? '--squash' : ''}")
+  lines = run("cd #{image[:name]} && docker build . --no-cache --tag #{image[:tag]} #{image[:squash] ? '--squash' : ''} #{image[:extra_args] ? image[:extra_args] : ''}")
   raise "Error building the image for #{image[:name]}: #{lines[-1]}" if lines[-1] =~ /successfully built/
 end
 
