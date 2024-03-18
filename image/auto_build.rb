a3cff3588c57aca7047923a351bd4e8a4e4190bf
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2024-03-18 13:07:31 +0800

    Use Github's ARM hosted runners for building ARM image
    Why this change?
    
    We have been given access to Github's private beta of ARM hosted
    runners. Switching to ARM runners should drastically speed up the time
    required for us to build our ARM image.

--- image/auto_build.rb
+++ image/auto_build.rb
@@ -19,14 +19,29 @@ images = {
     tag: "discourse/base:build",
     extra_args: "-f release.Dockerfile",
   },
+  base_arm64: {
+    name: "base",
+    tag: "discourse/base:build_arm64",
+    extra_args: "-f release.Dockerfile --platform linux/arm64 --build-arg=\"tag=build_slim_arm64\"",
+  },
   discourse_test_build: {
     name: "discourse_test",
     tag: "discourse/discourse_test:build",
+  },
+  discourse_test_buid_arm64: {
+    name: "discourse_test",
+    tag: "discourse/discourse_test:build_arm64",
+    extra_args: "--platform linux/arm64 --build-arg=\"from_tag=build_arm64\"",
   },
   discourse_dev: {
     name: "discourse_dev",
     tag: "discourse/discourse_dev:build",
   },
+  discourse_dev_arm64: {
+    name: "discourse_dev",
+    tag: "discourse/discourse_dev:build_arm64",
+    extra_args: "--platform linux/arm64",
+  }
 }
 
 def run(command)
